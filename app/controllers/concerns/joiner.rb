07cc3d97ca59557fec420a0c18c48e893543e65c
Joseph paul <josephpaul0487@gmail.com>
2020-05-29 18:53:41 +0530

    no message

--- app/controllers/concerns/joiner.rb
+++ app/controllers/concerns/joiner.rb
@@ -21,21 +21,23 @@ module Joiner
 
   # Displays the join room page to the user
   def show_user_join
-    # Get users name
-    @name = if current_user
-      current_user.name
-    elsif cookies.encrypted[:greenlight_name]
-      cookies.encrypted[:greenlight_name]
-    else
-      ""
-    end
+    @api = "sdfrsfsfsdfsdf"
+    render("api/api")
+    # # Get users name
+    # @name = if current_user
+    #   current_user.name
+    # elsif cookies.encrypted[:greenlight_name]
+    #   cookies.encrypted[:greenlight_name]
+    # else
+    #   ""
+    # end
 
-    @search, @order_column, @order_direction, pub_recs =
-      public_recordings(@room.bbb_id, params.permit(:search, :column, :direction), true)
+    # @search, @order_column, @order_direction, pub_recs =
+    #   public_recordings(@room.bbb_id, params.permit(:search, :column, :direction), true)
 
-    @pagy, @public_recordings = pagy_array(pub_recs)
+    # @pagy, @public_recordings = pagy_array(pub_recs)
 
-    render :join
+    # render :join
   end
 
   # create or update cookie to track the three most recent rooms a user joined
