709f9576cedb50aaf02e311dd846cc189b9ac2e1
Joseph paul <josephpaul0487@gmail.com>
2020-06-01 14:22:45 +0530

    no message

--- app/controllers/concerns/joiner.rb
+++ app/controllers/concerns/joiner.rb
@@ -61,7 +61,7 @@ module Joiner
         redirect_to join_path(@room, current_user.name, opts, current_user.uid)
       else
         join_name = params[:join_name] || params[@room.invite_path][:join_name]
-        @api = join_name
+        @api = params
         return render("api/api")
         # redirect_to join_path(@room, join_name, opts, fetch_guest_id)
       end
